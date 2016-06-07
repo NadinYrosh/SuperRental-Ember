43b59cbf043129c4c8400cfb43770c1eac1bd840
In Mahaphol and Nadiya Yeroshkina <in@>
2016-06-07 09:34:34 -0700

    Add update ability to the form.

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -9,13 +9,19 @@ export default Ember.Component.extend({
 
     save1() {
       var params = {
-        owner: this.get('owner'),
-        city: this.get('city'),
-        type: this.get('type'),
-        image: this.get('image'),
-        bedrooms: this.get('bedrooms'),
+        owner: this.get('owner') ? this.get('owner') : "",
+        city: this.get('city') ? this.get('city') : "",
+        type: this.get('type') ? this.get('type') : "",
+        image: this.get('image') ? this.get('image') : "",
+        bedrooms: this.get('bedrooms') ? this.get('bedrooms') :"",
       };
       this.set('addNewRental', false);
+    //   if(params.owner === "" || params.city === "" || params.type === "" || params.image === "" || params.bedrooms === ""){
+    //    return alert("Please enter something");
+    //  }else{
+    //    return params;
+    //  }
+
       this.sendAction('save2', params);
     }
   }
